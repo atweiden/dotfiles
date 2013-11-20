92b66c0aed9224a6dd86111914c1a62a0fbda675
Andy Weidenbaum <archbaum@gmail.com>
2013-11-19 18:13:37 -0800

    Add lafsify.sh

---
+++ _functions.d/functions/lafsify.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# -----------------------------------------------------------------------------
+# LAFSify: derive an Electrum wallet from any given Tahoe-LAFS writecap
+# -----------------------------------------------------------------------------
+
+function lafsify() {
+echo -n 'Enter your Tahoe-LAFS writecap: '; read WRITECAP
+SEED=$(hURL -s --no-color --HEX $WRITECAP)
+expect <<EOF
+  spawn electrum -w electrum-lafs.dat restore -o -C
+  expect "Password*" {
+    send "\r"
+  }
+  expect "fee*" {
+    send "\r"
+  }
+  expect "gap*" {
+    send "\r"
+  }
+  expect "seed*" {
+    send "$SEED\r"
+  }
+  expect eof
+EOF
+MNEMONIC=$(electrum -w electrum-lafs.dat getseed -o | jq -M '.mnemonic')
+echo "Your Tahoe-LAFS writecap: $WRITECAP"
+echo "Your resulting Electrum wallet seed: $SEED"
+echo "Your resulting Electrum wallet mnemonic: $MNEMONIC"
+}
