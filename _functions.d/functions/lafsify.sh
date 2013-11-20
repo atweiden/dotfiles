2c82a4cc6a2ab7f31ba24e0965667fa5cce65629
Andy Weidenbaum <archbaum@gmail.com>
2013-11-19 18:26:58 -0800

    add unlafsify function

--- _functions.d/functions/lafsify.sh
+++ _functions.d/functions/lafsify.sh
@@ -28,3 +28,28 @@ echo "Your Tahoe-LAFS writecap: $WRITECAP"
 echo "Your resulting Electrum wallet seed: $SEED"
 echo "Your resulting Electrum wallet mnemonic: $MNEMONIC"
 }
+
+function unlafsify() {
+echo -n 'Enter your Electrum-LAFS mnemonic: '; read MNEMONIC
+expect <<EOF
+  spawn electrum -w electrum-lafs.dat restore -o -C
+  expect "Password*" {
+    send "\r"
+  }
+  expect "fee*" {
+    send "\r"
+  }
+  expect "gap*" {
+    send "\r"
+  }
+  expect "seed*" {
+    send "$MNEMONIC\r"
+  }
+  expect eof
+EOF
+SEED=$(electrum -w electrum-lafs.dat getseed -o | jq -M '.seed')
+WRITECAP=$(hURL -s --no-color --hex $SEED)
+echo "Your starting Electrum wallet mnemonic: $MNEMONIC"
+echo "Your starting Electrum wallet seed: $SEED"
+echo "Your resulting Tahoe-LAFS writecap: $WRITECAP"
+}
