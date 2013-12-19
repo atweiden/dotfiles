c689b242c4bb683df8b77feec60f5eb51a884685
Andy Weidenbaum <archbaum@gmail.com>
2013-12-19 02:05:45 -0800

    revamp bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,20 @@
 #! /bin/bash
 
-# Setup basics, get Vundle
-mkdir -p $HOME/.src/dotfiles/_vim{/.backups,/.swaps,/.tmp,}
-git clone https://github.com/gmarik/vundle $HOME/.src/dotfiles/_vim/bundle/vundle
+# -----------------------------------------------------------------------------
+# bootstrap: quick and easy dotfiles setup
+# -----------------------------------------------------------------------------
 
-# Backup important dotfiles
+
+# -----------------------------------------------------------------------------
+# basics
+
+mkdir -p $HOME/.vim/{.backups,.swaps,.tmp,}
+git clone https://github.com/gmarik/vundle $HOME/.vim/bundle/vundle
+
+
+# -----------------------------------------------------------------------------
+# backup
+
 for dotfile in $HOME/.ackrc \
                $HOME/.bashrc \
                $HOME/.bash_logout \
@@ -30,41 +40,49 @@ for dotfile in $HOME/.ackrc \
                $HOME/.vimencrypt \
                $HOME/.Xdefaults \
                $HOME/.xinitrc \
-               $HOME/.xsession; do mv $dotfile ${dotfile}.bak;
+               $HOME/.xsession; do echo "backing up $dotfile (if it exists)"
+                                   mv $dotfile ${dotfile}.bak 2&>/dev/null;
 done
 
-# Setup symbolic links
-ln -s $HOME/.src/dotfiles/_ackrc $HOME/.ackrc
-ln -s $HOME/.src/dotfiles/_bashrc-local $HOME/.bashrc
-ln -s $HOME/.src/dotfiles/_bash_logout $HOME/.bash_logout
-ln -s $HOME/.src/dotfiles/_bash_profile $HOME/.bash_profile
-ln -s $HOME/.src/dotfiles/_config $HOME/.config
-ln -s $HOME/.src/dotfiles/_conkyrc $HOME/.conkyrc
-ln -s $HOME/.src/dotfiles/_conkyrc1 $HOME/.conkyrc1
-ln -s $HOME/.src/dotfiles/_dunstrc $HOME/.dunstrc
-ln -s $HOME/.src/dotfiles/_functions.d $HOME/.functions.d
-ln -s $HOME/.src/dotfiles/_gitignore $HOME/.gitignore
-ln -s $HOME/.src/dotfiles/_gitconfig $HOME/.gitconfig
-ln -s $HOME/.src/dotfiles/_gitattributes $HOME/.gitattributes
-ln -s $HOME/.src/dotfiles/_gnupg $HOME/.gnupg
-ln -s $HOME/.src/dotfiles/_hgignore $HOME/.hgignore
-ln -s $HOME/.src/dotfiles/_hgrc $HOME/.hgrc
-ln -s $HOME/.src/dotfiles/_iex $HOME/.iex
-ln -s $HOME/.src/dotfiles/_jshintrc $HOME/.jshintrc
-ln -s $HOME/.src/dotfiles/_psqlrc $HOME/.psqlrc
-ln -s $HOME/.src/dotfiles/_screenrc $HOME/.screenrc
-ln -s $HOME/.src/dotfiles/_tmuxconf $HOME/.tmux.conf
-ln -s $HOME/.src/dotfiles/_vim $HOME/.vim
-ln -s $HOME/.src/dotfiles/_vimrc $HOME/.vimrc
-ln -s $HOME/.src/dotfiles/_vimencrypt $HOME/.vimencrypt
-ln -s $HOME/.src/dotfiles/_Xdefaults $HOME/.Xdefaults
-ln -s $HOME/.src/dotfiles/_xinitrc $HOME/.xinitrc
-chmod +x $HOME/.xinitrc
-ln -s $HOME/.src/dotfiles/_xsession $HOME/.xsession
-chmod +x $HOME/.xsession
 
-# Configure f.lux
-echo "f.lux needs your zipcode for screen warmth timing"
+# -----------------------------------------------------------------------------
+# f.lux
+
+echo "f.lux requests your zipcode for screen warmth timing"
 echo -n "Enter your zipcode: "
 read zipcode
 sed -i "s#97210#$zipcode#" _config/openbox/autostart.sh
+
+
+# -----------------------------------------------------------------------------
+# links
+
+for dotfile in _ackrc \
+               _bashrc-local \
+               _bash_logout \
+               _bash_profile \
+               _config \
+               _conkyrc \
+               _conkyrc1 \
+               _dunstrc \
+               _functions.d \
+               _gitignore \
+               _gitconfig \
+               _gitattributes \
+               _gnupg \
+               _hgignore \
+               _hgrc \
+               _iex \
+               _jshintrc \
+               _psqlrc \
+               _screenrc \
+               _tmux \
+               _vim \
+               _vimrc \
+               _vimencrypt \
+               _Xdefaults \
+               _xinitrc \
+               _xsession; do SRC=\"$(readlink -f $dotfile)\"
+                                  DEST=\"$HOME/.${SRC##*_}\"
+                                          ln -s $SRC $DEST;
+done
