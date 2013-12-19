49475a7291b71b05b739b1c6599fc7706d64f66f
Andy Weidenbaum <archbaum@gmail.com>
2013-12-18 19:12:33 -0800

    move packagemaintenance to function

---
+++ _functions.d/functions/packagemaintenance.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# -----------------------------------------------------------------------------
+# packagemaintenance: check all binaries in /usr/bin for any with "not
+#                     found" library links
+#
+# Source: https://bbs.archlinux.org/viewtopic.php?pid=1143655#p1143655
+# -----------------------------------------------------------------------------
+
+function packagemaintenance() {
+cd /usr/bin
+for file in $(find .  -type f -executable -readable)
+do
+    ldd $file | grep "not found" >/dev/null && echo -n $file " links to an missing library " && \
+    echo "(rebuild `pacman -Qq --owns $file`)"
+done
+}
