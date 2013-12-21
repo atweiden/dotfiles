a4c20f078e5e83e1783fe373d49dea374c4a6272
Andy Weidenbaum <archbaum@gmail.com>
2013-12-21 13:26:10 -0800

    Add aurinstall.sh

---
+++ _functions.d/functions/aurinstall.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# -----------------------------------------------------------------------------
+# aurinstall: easily install one or more packages from the AUR
+# -----------------------------------------------------------------------------
+
+function aurinstall() {
+ORIGIN=$PWD
+mkdir -p ~/.src
+for aurpkg in "$@"; do 
+  cd ~/.src
+  rm -rf ${aurpkg} ${aurpkg}.tar.gz*
+  wget https://aur.archlinux.org/packages/${aurpkg:0:2}/${aurpkg}/${aurpkg}.tar.gz
+  mkdir ${aurpkg}
+  tar -xvzf ${aurpkg}.tar.gz --strip 1 -C ${aurpkg}
+  cd ${aurpkg}
+  makepkg -Acsi --noconfirm
+done
+cd $ORIGIN
+}
