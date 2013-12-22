aab8bfd55c76da94b7b2e4a9531ec3ba7bee5e71
Andy Weidenbaum <archbaum@gmail.com>
2013-12-22 14:46:15 -0800

    no need for local file

--- _functions.d/functions.sh
+++ _functions.d/functions.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-local file; for file in `find ~/.functions.d/functions -type f -name "*.sh"`; do . $file; done
+for fnsh in `find ~/.functions.d/functions -type f -name "*.sh"`; do . $fnsh; done
