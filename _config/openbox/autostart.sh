f25cccb4f1f22e8fcfcdfb253a06c69f2639133e
Andy Weidenbaum <archbaum@gmail.com>
2014-01-31 23:51:43 -0800

    slam screensaver

--- _config/openbox/autostart.sh
+++ _config/openbox/autostart.sh
@@ -4,7 +4,7 @@ conky -q &
 volumeicon &
 batti &
 nitrogen --restore &
-xautolock -time 7 -locker "/usr/bin/xlock -mode blank"  -detectsleep &
+xautolock -secure -time 7 -locker "/usr/bin/xlock -logoutButton 1 -logoutCmd slam -logoutButtonLabel 'Slam' -mode blank"  -detectsleep &
 xflux -z 97210 > /dev/null 2>&1 &
 pcmanfm -d &
 #(sleep 3 && /usr/bin/nm-applet --sm-disable) &
