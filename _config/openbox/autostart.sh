2498b879db24a52f4b27c7c824cdb4e7d71b7eba
Andy Weidenbaum <archbaum@gmail.com>
2014-04-16 09:17:45 -0700

    valabattery starts too quickly

--- _config/openbox/autostart.sh
+++ _config/openbox/autostart.sh
@@ -4,9 +4,9 @@ conky -q &
 volumeicon &
 nitrogen --restore &
 xautolock -secure -time 7 -locker "/usr/bin/xlock -mode blank"  -detectsleep &
-valabattery &
 xflux -z 97210 > /dev/null 2>&1 &
 pcmanfm -d &
+(sleep 3 && valabattery) &
 #(sleep 3 && /usr/bin/nm-applet --sm-disable) &
 #xcompmgr &
 #xbacklight -set 100 &
