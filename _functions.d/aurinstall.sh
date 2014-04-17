237328c989dad95c4b26eb2939cf8e19b97efe26
Andy Weidenbaum <archbaum@gmail.com>
2014-04-17 07:46:19 -0700

    aurinstall

--- _functions.d/aurinstall.sh
+++ _functions.d/aurinstall.sh
@@ -6,13 +6,13 @@
 
 function aurinstall() {
 ORIGIN=$PWD
-aurpkgs=$(< /dev/stdin)
+read -a aurpkgs <<< "$(< /dev/stdin)"
 mkdir -p ~/.src
 for aurpkg in "$@" "$aurpkgs"; do
   cd ~/.src
   rm -rf ${aurpkg} ${aurpkg}.tar.gz*
-  wget https://aur.archlinux.org/packages/${aurpkg:0:2}/${aurpkg}/${aurpkg}.tar.gz
-  mkdir ${aurpkg}
+  curl -O https://aur.archlinux.org/packages/${aurpkg:0:2}/${aurpkg}/${aurpkg}.tar.gz
+  mkdir -p ${aurpkg}
   tar -xvzf ${aurpkg}.tar.gz --strip 1 -C ${aurpkg}
   cd ${aurpkg}
   makepkg -Acsi --noconfirm
