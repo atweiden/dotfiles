daeabde0628fa855b733667f88dbede9478da963
Andy Weidenbaum <archbaum@gmail.com>
2014-10-08 20:04:15 -0700

    bash functions

--- _functions.d/getdirs.sh
+++ _functions.d/getdirs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # -----------------------------------------------------------------------------
-# get[a]dirs: list [all] directories
+# get[a]dirs[o]: list [all] directories [only '.' dirs]
 # -----------------------------------------------------------------------------
 
 normal=$(tput sgr0)
@@ -19,5 +19,12 @@ function getadirs() {
 declare _adirs=$(find "$@" -mindepth 1 -maxdepth 1 -type d | sort | sed 's@^./@@' | awk -F'/' '{print $NF}')
 for i in ${!_adirs[@]}; do
   echo "${bold}${blue}${_adirs[${i}]}${normal}" | sed 's@$@/@'
+done
+}
+
+function getadirso() {
+declare _adirso=$(find "$@" -mindepth 1 -maxdepth 1 -type d -name "*.*" | sort | sed 's@^./@@' | awk -F'/' '{print $NF}')
+for i in ${!_adirso[@]}; do
+  echo "${bold}${blue}${_adirso[${i}]}${normal}" | sed 's@$@/@'
 done
 }
