76df0061d48e731b893bbd8c1b263985395833f2
Andy Weidenbaum <archbaum@gmail.com>
2014-10-18 15:32:43 -0700

    use compton

--- _config/openbox/autostart.sh
+++ _config/openbox/autostart.sh
@@ -7,8 +7,9 @@ xautolock -secure -time 7 -locker "/usr/bin/xlock -mode blank"  -detectsleep &
 xflux -z 97210 > /dev/null 2>&1 &
 pcmanfm -d &
 (sleep 3 && valabattery) &
+#compton --config ~/.config/compton.conf -b &
+compton -cCGb &
 #(sleep 3 && /usr/bin/nm-applet --sm-disable) &
-#xcompmgr &
 #xbacklight -set 100 &
 #/usr/bin/synclient TouchpadOff=1 &
 #fbxkb &
