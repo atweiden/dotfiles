3f5166ca0bb4d6363bb601a705eadb8840236026
Andy Weidenbaum <archbaum@gmail.com>
2014-10-19 11:25:51 -0700

    improve script resiliency

--- _config/openbox/autostart.sh
+++ _config/openbox/autostart.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-tint2 &
-conky -q &
-volumeicon &
-nitrogen --restore &
-xautolock -secure -time 7 -locker "/usr/bin/xlock -mode blank"  -detectsleep &
-xflux -z 97210 > /dev/null 2>&1 &
-pcmanfm -d &
-(sleep 3 && valabattery) &
+[[ -x /usr/bin/tint2 ]] && tint2 &
+[[ -x /usr/bin/conky ]] && conky -q &
+[[ -x /usr/bin/volumeicon ]] && volumeicon &
+[[ -x /usr/bin/nitrogen ]] && nitrogen --restore &
+[[ -x /usr/bin/xautolock && -x /usr/bin/xlock ]] \
+  && xautolock -secure -time 7 -locker "/usr/bin/xlock -mode blank"  -detectsleep &
+[[ -x /usr/bin/xflux ]] && xflux -z 97210 > /dev/null 2>&1 &
+[[ -x /usr/bin/pcmanfm ]] && pcmanfm -d &
+[[ -x /usr/bin/valabattery ]] && (sleep 3 && valabattery) &
+[[ -x /usr/bin/compton ]] && compton --config ~/.config/compton/compton.conf -b &
 #compton -cCGb &
-compton --config ~/.config/compton/compton.conf -b &
 #(sleep 3 && /usr/bin/nm-applet --sm-disable) &
 #xbacklight -set 100 &
 #/usr/bin/synclient TouchpadOff=1 &
