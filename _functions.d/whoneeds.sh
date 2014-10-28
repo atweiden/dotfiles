687071ff829f28f13a204732d037335f01cfbb94
Andy Weidenbaum <archbaum@gmail.com>
2014-10-28 15:12:01 -0700

    Added whoneeds/whowants function

---
+++ _functions.d/whoneeds.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# -----------------------------------------------------------------------------
+# whoneeds: display arch packages that depend on another
+# -----------------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------------
+# colors
+
+normal=$(tput sgr0)
+bold=$(tput bold)
+green=$(tput setaf 2)
+white=$(tput setaf 7)
+
+
+# -----------------------------------------------------------------------------
+# depends
+
+function whoneeds() {
+for _basepkg in "$@"; do
+  echo "${bold}${white}Packages that depend on${normal} ${green}${_basepkg}${normal}:"
+  for _pkg in `pacman -Qi "${_basepkg}" | ag required | sed 's@.*:\s@@'`; do
+    echo "$_pkg"
+  done
+  echo ""
+done
+}
+
+
+# -----------------------------------------------------------------------------
+# optdepends
+
+function whowants() {
+for _basepkg in "$@"; do
+  echo "${bold}${white}Packages that optionally depend on${normal} ${green}${_basepkg}${normal}:"
+  for _pkg in `pacman -Qi "${_basepkg}" | ag "optional for" | sed 's@.*:\s@@'`; do
+    echo "$_pkg"
+  done
+  echo ""
+done
+}
