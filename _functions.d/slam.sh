31df61825623ff2298287f2d8a2a9db5d73dbaec
Andy Weidenbaum <archbaum@gmail.com>
2014-12-22 16:48:40 -0800

    put ~/.vim/view in quixand, created by vim mkview

--- _functions.d/slam.sh
+++ _functions.d/slam.sh
@@ -5,5 +5,19 @@
 # -----------------------------------------------------------------------------
 
 function slam() {
-quixand -p ~/.bash_history ~/.lesshst ~/.local/share/recently-used.xbel ~/.viminfo ~/.vim_mru_files -d ~/Sandbox ~/.cache ~/.thumbnails ~/.vim/.backups ~/.vim/.swaps ~/.vim/.tmp ~/.vim/.undo && sudo shutdown now
+[[ -x /usr/bin/quixand ]] \
+  && quixand -p ~/.bash_history \
+                ~/.lesshst \
+                ~/.local/share/recently-used.xbel \
+                ~/.viminfo \
+                ~/.vim_mru_files \
+                -d ~/Sandbox \
+                   ~/.cache \
+                   ~/.thumbnails \
+                   ~/.vim/.backups \
+                   ~/.vim/.swaps \
+                   ~/.vim/.tmp \
+                   ~/.vim/.undo \
+                   ~/.vim/view
+sudo shutdown now
 }
--- _functions.d/slur.sh
+++ _functions.d/slur.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
 # -----------------------------------------------------------------------------
-# slam: safe reboot
+# slur: safe reboot
 # -----------------------------------------------------------------------------
 
 function slur() {
-quixand -p ~/.bash_history ~/.lesshst ~/.local/share/recently-used.xbel ~/.viminfo ~/.vim_mru_files -d ~/Sandbox ~/.cache ~/.thumbnails ~/.vim/.backups ~/.vim/.swaps ~/.vim/.tmp ~/.vim/.undo && sudo reboot
+[[ -x /usr/bin/quixand ]] \
+  && quixand -p ~/.bash_history \
+                ~/.lesshst \
+                ~/.local/share/recently-used.xbel \
+                ~/.viminfo \
+                ~/.vim_mru_files \
+                -d ~/Sandbox \
+                   ~/.cache \
+                   ~/.thumbnails \
+                   ~/.vim/.backups \
+                   ~/.vim/.swaps \
+                   ~/.vim/.tmp \
+                   ~/.vim/.undo \
+                   ~/.vim/view
+sudo reboot
 }
