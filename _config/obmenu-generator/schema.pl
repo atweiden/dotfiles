6a157757b1b4750727697ca799c371b3fc6fea52
Andy Weidenbaum <archbaum@gmail.com>
2015-01-29 09:12:13 -0800

    update obmenu-generator

--- _config/obmenu-generator/schema.pl
+++ _config/obmenu-generator/schema.pl
@@ -1,60 +1,38 @@
 #!/usr/bin/perl
 
-# SCHEMA supports the following keys: item, cat, begin_cat, end_cat,
-#                                     exit, raw, sep, obgenmenu
+# obmenu-generator - schema file
 
 =for comment
 
-item: add an item into the menu
-{item => ["command", "label", "icon"]}
-
-cat: add a category into the menu
-{cat => ["name", "label", "icon"]}
-
-begin_cat: begin of a category
-{begin_cat => ["name", "icon"]}
-
-end_cat: end of a category
-{end_cat => undef}
-
-sep: menu line separator
-{sep => undef} or {sep => "label"}
-
-exit: default "Exit" action
-{exit => ["label", "icon"]}
-
-raw: any valid Openbox XML string
-{raw => q(xml string)},
-
-obgenmenu: category provided by obmenu-generator
-{obgenmenu => "label"}
-
-scripts: executable scripts from a directory
-{scripts => ["/my/dir", BOOL, "icon"]}
-BOOL - can be either true or false (1 or 0)
-0 == open the script in background
-1 == open the script in a new terminal
-
-wine_apps: windows applications installed via wine
-{wine_apps => ["label", "icon"]}
+    item:      add an item inside the menu               {item => ["command", "label", "icon"]},
+    cat:       add a category inside the menu             {cat => ["name", "label", "icon"]},
+    sep:       horizontal line separator                  {sep => undef}, {sep => "label"},
+    pipe:      a pipe menu entry                         {pipe => ["command", "label", "icon"]},
+    raw:       any valid Openbox XML string               {raw => q(xml string)},
+    begin_cat: begin of a category                  {begin_cat => ["name", "icon"]},
+    end_cat:   end of a category                      {end_cat => undef},
+    obgenmenu: generic menu settings                {obgenmenu => ["label", "icon"]},
+    exit:      default "Exit" action                     {exit => ["label", "icon"]},
 
 =cut
 
 # NOTE:
 #    * Keys and values are case sensitive. Keep all keys lowercase.
-#    * ICON can be a either a direct path to a icon or a valid icon name
-#    * By default, category names are case insensitive. (e.g.: X-XFCE == x_xfce)
+#    * ICON can be a either a direct path to an icon or a valid icon name
+#    * Category names are case insensitive. (X-XFCE and x_xfce are equivalent)
 
-require '/home/lniux/.config/obmenu-generator/config.pl';
+require "$ENV{HOME}/.config/obmenu-generator/config.pl";
+
+## Text editor
+my $editor = $CONFIG->{editor};
 
 our $SCHEMA = [
-    #             COMMAND                 LABEL                ICON
-    {item => ['pcmanfm',             'File Manager',      'file-manager']},
-    {item => [$CONFIG->{terminal},   'Terminal',          'terminal']},
-    {item => [$CONFIG->{editor},     'Editor',            'text-editor']},
-    {item => ['firefox',             'Web Browser',       'web-browser']},
-    {item => ['gmrun',               'Run command',       'system-run']},
-    {item => ['pidgin',              'Instant messaging', 'system-users']},
+    #          COMMAND                 LABEL                ICON
+    {item => ['pcmanfm',           'File Manager',      'file-manager']},
+    {item => [$CONFIG->{terminal}, 'Terminal',          'terminal']},
+    {item => ['firefox',           'Web Browser',       'web-browser']},
+    {item => ['gmrun',             'Run command',       'system-run']},
+    {item => ['pidgin',            'Instant messaging', 'system-users']},
 
     {sep => 'Applications'},
 
@@ -67,10 +45,11 @@ our $SCHEMA = [
     {cat => ['audiovideo',  'Multimedia',  'applications-multimedia']},
     {cat => ['network',     'Network',     'applications-internet']},
     {cat => ['office',      'Office',      'applications-office']},
+    {cat => ['other',       'Other',       'applications-other']},
     {cat => ['settings',    'Settings',    'applications-accessories']},
     {cat => ['system',      'System',      'applications-system']},
 
-    #{cat => ['qt',          'QT Applications',    'qtlogo']},
+    #{cat => ['qt',          'QT Applications',    'qt4logo']},
     #{cat => ['gtk',         'GTK Applications',   'gnome-applications']},
     #{cat => ['x_xfce',      'XFCE Applications',  'applications-other']},
     #{cat => ['gnome',       'GNOME Applications', 'gnome-applications']},
@@ -81,17 +60,57 @@ our $SCHEMA = [
     #             ... some items ...
     #{end_cat   => undef},
 
-    #                  LABEL             ICON
-    #{wine_apps => ['Wine apps', 'applications-other']},
+    #            COMMAND     LABEL        ICON
+    #{pipe => ['obbrowser', 'Disk', 'drive-harddisk']},
 
-    #                DIR     BOOL       ICON
-    #{scripts => ['/my/path', 1,  'text-x-script']},
+    ## Generic advanced settings
+    #{sep       => undef},
+    #{obgenmenu => ['Openbox Settings', 'openbox']},
+    #{sep       => undef},
 
-    {obgenmenu => 'Openbox Settings'},
-    {sep       => undef},
+    ## Custom advanced settings
+    {sep => undef},
+    {begin_cat => ['Advanced Settings', 'gnome-settings']},
 
-    {item => ['xlockmore', 'Lock', 'lock']},
+        # Configuration files
+        {item      => ["$editor ~/.conkyrc",              'Conky RC',    $editor]},
+        {item      => ["$editor ~/.config/tint2/tint2rc", 'Tint2 Panel', $editor]},
 
-    # This options uses the default OpenBox action "Exit"
+        # obmenu-generator category
+        {begin_cat => ['Obmenu-Generator', 'menu-editor']},
+            {item      => ["$editor ~/.config/obmenu-generator/schema.pl", 'Menu Schema', $editor]},
+            {item      => ["$editor ~/.config/obmenu-generator/config.pl", 'Menu Config', $editor]},
+
+            {sep  => undef},
+            {item => ['obmenu-generator -p',    'Generate a pipe menu',              'menu-editor']},
+            {item => ['obmenu-generator -s',    'Generate a static menu',            'menu-editor']},
+            {item => ['obmenu-generator -p -i', 'Generate a pipe menu with icons',   'menu-editor']},
+            {item => ['obmenu-generator -s -i', 'Generate a static menu with icons', 'menu-editor']},
+            {sep  => undef},
+
+            {item    => ['obmenu-generator -d', 'Refresh Icon Set', 'gtk-refresh']},
+        {end_cat => undef},
+
+        # Openbox category
+        {begin_cat => ['Openbox', 'openbox']},
+            {item      => ['openbox --reconfigure',               'Reconfigure Openbox', 'openbox']},
+            {item      => ["$editor ~/.config/openbox/autostart", 'Openbox Autostart',    $editor]},
+            {item      => ["$editor ~/.config/openbox/rc.xml",    'Openbox RC',           $editor]},
+            {item      => ["$editor ~/.config/openbox/menu.xml",  'Openbox Menu',         $editor]},
+        {end_cat => undef},
+
+        # Pacman category
+        {begin_cat => ['Pacman / Servers', 'package-manager-icon']},
+            {item      => ["gksu $editor /etc/makepkg.conf",        'Makepkg Config',    'start-here']},
+            {item      => ["gksu $editor /etc/pacman.conf",         'Pacman Config',     'start-here']},
+            {item      => ["gksu $editor /etc/pacman.d/mirrorlist", 'Pacman Mirrorlist', 'start-here']},
+        {end_cat => undef},
+    {end_cat => undef},
+    {sep => undef},
+
+    ## The screen lock command
+    {item => ['xlock -mode blank', 'Lock', 'lock']},
+
+    # This option uses the default Openbox's action "Exit"
     {exit => ['Exit', 'exit']},
 ]
