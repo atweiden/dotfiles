836587bb66055a2ea52beab89e3d4209a4943731
Andy Weidenbaum <archbaum@gmail.com>
2015-01-28 23:58:53 -0800

    add 256 color term test

---
+++ _functions.d/colors.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# -----------------------------------------------------------------------------
+# colors: print all 256 colors for testing
+# http://www.commandlinefu.com/commands/view/6533/print-all-256-colors-for-testing-term-or-for-a-quick-reference
+# -----------------------------------------------------------------------------
+
+function colors() {
+( x=$(tput op) y=$(printf %$((${COLUMNS}-6))s);
+  for i in {0..256}; do
+    o=00$i;
+    echo -e ${o:${#o}-3:3} $(tput setaf $i; tput setab $i)${y// /=}$x;
+  done )
+}
