232d682517593f0458de817694cb237c6c9a5f67
Andy Weidenbaum <archbaum@gmail.com>
2015-04-27 12:39:05 -0700

    emod: open files in mercurial repo with uncommitted changes

---
+++ _functions.d/emod.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# -----------------------------------------------------------------------------
+# emod: open files in mercurial repo with uncommitted changes
+# -----------------------------------------------------------------------------
+
+function emod() {
+declare _modified=($(hg st | perl -n -e 'if (/^M\b(.*)$/) { print "$1\n" }' | awk '{print $1}'))
+$EDITOR "${_modified[@]}"
+}
