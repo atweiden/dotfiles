3d7af73d47b528dcbcb5965fb64d49a9b10f8877
Andy Weidenbaum <archbaum@gmail.com>
2015-07-21 08:51:15 -0700

    built on

--- _functions.d/pkgdate.sh
+++ _functions.d/pkgdate.sh
@@ -9,7 +9,7 @@ function pkgdate() {
 if [[ -x /usr/bin/perl6 ]]; then
 package=$1
 printf "%b" "$(pacman -Q $package) ("
-printf "%b" "$(pacman -Qi $package \
+printf "%b" "Built on $(pacman -Qi $package \
   | perl6 -ne '.say if m:s/Build Date/' \
   | perl6 -ne '.split(<:>, 2)[1].trim.say'))\n"
 else
