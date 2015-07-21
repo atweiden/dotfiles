ba889dc305c1fdec874c0c3340969619f596b4e5
Andy Weidenbaum <archbaum@gmail.com>
2015-07-21 07:51:56 -0700

    added output if missing binary

--- _functions.d/pkgdate.sh
+++ _functions.d/pkgdate.sh
@@ -2,6 +2,7 @@
 
 # -----------------------------------------------------------------------------
 # pkgdate: display version and build date of package
+#          panda-git 20150613-1 (Thu 18 Jun 2015 04:45:03 PM PDT)
 # -----------------------------------------------------------------------------
 
 function pkgdate() {
@@ -11,6 +12,7 @@ printf "%b" "$(pacman -Q $package) ("
 printf "%b" "$(pacman -Qi $package \
   | perl6 -ne '.say if m:s/Build Date/' \
   | perl6 -ne '.split(<:>, 2)[1].trim.say'))\n"
-# panda-git 20150613-1 (Thu 18 Jun 2015 04:45:03 PM PDT)
+else
+echo "Missing Perl6 binary at 「/usr/bin/perl6」"
 fi
 }
