129b62e5e78a5ae19875794cbb119c9f99885c10
Andy Weidenbaum <archbaum@gmail.com>
2015-08-30 21:44:16 -0700

    rename lc to lcf, lc conflicts with mono binary of the same name

--- _functions.d/lcf.sh
+++ _functions.d/lcf.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # -----------------------------------------------------------------------------
-# lc: count number of files
+# lcf: count number of files
 # https://news.ycombinator.com/item?id=5023277
 # -----------------------------------------------------------------------------
 
 
 # -----------------------------------------------------------------------------
-# `lc /dir`  : counts the number of files in /dir
-# `lc /dir/*`: counts the files in subdirectories of /dir.
+# `lcf /dir`  : counts the number of files in /dir
+# `lcf /dir/*`: counts the files in subdirectories of /dir.
 
-lc() { if [[ "$#" -gt 1 ]]; then for DIR in "$@"; do echo -n "$DIR - " ; ls -AU1 $DIR | wc -l ; done ; else ls -AU1 "$@" | wc -l ; fi; }
+lcf() { if [[ "$#" -gt 1 ]]; then for DIR in "$@"; do echo -n "$DIR - " ; ls -AU1 $DIR | wc -l ; done ; else ls -AU1 "$@" | wc -l ; fi; }
