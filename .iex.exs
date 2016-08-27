16fd141a924cf7aa804b84de5d742423a0e3339a
Andy Weidenbaum <archbaum@gmail.com>
2016-08-26 21:37:28 -0700

    get elixir iex hot code reloading
    http://stackoverflow.com/q/32540703

--- .iex.exs
+++ .iex.exs
@@ -1,1 +1,10 @@
 IEx.configure colors: [ eval_result: [:cyan,:bright] ]
+
+defmodule R do
+  def reload! do
+    Mix.Task.reenable "compile.elixir"
+    Application.stop(Mix.Project.config[:app])
+    Mix.Task.run "compile.elixir"
+    Application.start(Mix.Project.config[:app], :permanent)
+  end
+end
